7dbc7c5e83147d01b152ebcfd21c5bf1ba571cd9
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-09-01 11:01:13 -0400

    played around with buttons

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,4 +1,7 @@
 class Signup < ApplicationRecord
     belongs_to :camper
     belongs_to :activity
+
+    validates :time, numericality:{only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 23 }
+
 end
